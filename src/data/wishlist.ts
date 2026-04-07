a0c7dfd4a3526e1a253039ed9c9141f63b5c7bb6
Will Jackson <wj@wovenandwoods.com>
2026-04-07 15:12:52 +0100

    Added item

--- src/data/wishlist.ts
+++ src/data/wishlist.ts
@@ -9,7 +9,7 @@ export interface WishlistItem {
 }
 
 export const meta = {
-  title: "Will's Wishlist",
+  title: "Wishlist 2026",
 };
 
 export const wishlist: WishlistItem[] = [
@@ -18,7 +18,7 @@ export const wishlist: WishlistItem[] = [
     brand: "Finisterre",
     description: "Organic cotton twill camp cap with diamond logo patch in navy",
     url: "https://finisterre.com/products/organic-cotton-diamond-twill-camp-cap-navy",
-    price: "£40",
+    price: "£40.00",
     priority: "medium"
   },
   {
@@ -26,7 +26,7 @@ export const wishlist: WishlistItem[] = [
     brand: "Safefoam",
     description: "A set of cushions and covers for an Ercol 334 chair",
     url: "https://www.safefoam.co.uk/product/ercol-334-seat-back-cushions-subtle-delaney-green/#SID=70",
-    price: "£185",
+    price: "£185.00",
     priority: "medium"
   },
   {
@@ -34,7 +34,7 @@ export const wishlist: WishlistItem[] = [
     brand: "Finisterre",
     description: "Reusable 1 litre bottle in seafoam blue and mermaid print",
     url: "https://finisterre.com/products/nalgene-sustain-1l-waterbottle-seafoam",
-    price: "£25",
+    price: "£25.00",
     priority: "medium"
   },
   {
@@ -42,7 +42,7 @@ export const wishlist: WishlistItem[] = [
     brand: "Finisterre",
     description: "Wool blend ribbed sock in whichever colour you think is nicest",
     url: "https://finisterre.com/products/wool-blend-ribbed-sock-beeswax",
-    price: "£20",
+    price: "£20.00",
     priority: "medium"
   },
   {
@@ -50,7 +50,7 @@ export const wishlist: WishlistItem[] = [
     brand: "Darn Tough",
     description: "A nice pair of decent socks",
     url: "https://darntough.uk/collections/mens-lifestyle-socks-dress-casual",
-    price: "£28",
+    price: "£28.00",
     priority: "medium"
   },
   {
@@ -58,7 +58,7 @@ export const wishlist: WishlistItem[] = [
     brand: "OXO",
     description: "A good box grater",
     url: "https://www.johnlewis.com/oxo-good-grips-box-grater/p230448461",
-    price: "£16",
+    price: "£16.00",
     priority: "medium"
   },
   {
@@ -66,7 +66,15 @@ export const wishlist: WishlistItem[] = [
     brand: "OXO",
     description: "A good tin opener",
     url: "https://www.amazon.co.uk/dp/B00004OCJW?ascsubtag=5121833",
-    price: "£12",
+    price: "£12.00",
+    priority: "medium"
+  },
+  {
+    title: "TM-02 Digital Kitchen Timer",
+    brand: "ThermPro",
+    description: "A dual-display digital kitchen timer",
+    url: "https://www.amazon.co.uk/ThermoPro-TM02-Countdown-Adjustable-Backlight/dp/B0CS35GXJK",
+    price: "£12.70",
     priority: "medium"
   }
   ];
